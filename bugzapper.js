ae68ea9e4fe940fd5653553358a33a0beb5d0b6d
Ryan Duan <duanpanda@gmail.com>
2016-10-03 21:40:18 -0400

    bacteria animation works

--- bugzapper.js
+++ bugzapper.js
@@ -1,13 +1,27 @@
 var canvas;
 var gl;
+var program;
 var vertices = [];
 var index = [];
 var colors = [];
-var numPoints = 80; // number of points per circle
+var vertexBuffer = null;
+var colorBuffer = null;
+var indexBuffer = null;
+var numPoints = 100; // number of points per circle
 var baseColors = [
-    vec3(0.7, 0.9, 0.3),
-    vec3(0.8, 0.2, 0.2)
+    vec3(0.0, 0.0, 0.0), // black
+    vec3(1.0, 0.0, 0.0), // red
+    vec3(1.0, 1.0, 0.0), // yellow
+    vec3(0.0, 1.0, 0.0), // green
+    vec3(0.0, 0.0, 1.0), // blue
+    vec3(1.0, 0.0, 1.0), // magenta
+    vec3(0.0, 1.0, 1.0), // cyan
+    vec3(0.7, 0.9, 0.3), // yellow-green
+    vec3(0.8, 0.2, 0.2)	 // dark red
 ];
+
+var bactIndex = 1;
+var maxBacts = 50;
 
 window.onload = function init()
 {
@@ -24,16 +38,10 @@ window.onload = function init()
 
     var disc = new Circle(vec3(0.0, 0.0, 0.0), // center coordinates
 			  0.8,		       // radius
-			  0,		       // color index for baseColors
+			  7,		       // color index for baseColors
 			  0);		       // z
     addObject(disc);
-    for (var i = 10; i < 20; i++) {
-	var bacteria = new Circle(disc.points[i],
-				  0.1,
-				  1,
-				  -1);
-	addObject(bacteria);
-    }
+    var intervalID = window.setInterval(genBacteria, 100, disc);
 
     //
     //  Configure WebGL
@@ -44,14 +52,14 @@ window.onload = function init()
 
     //  Load shaders and initialize attribute buffers
 
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
     // Load the data into the GPU
 
     // vertex coordinates
 
-    var vertexBuffer = gl.createBuffer();
+    vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
@@ -61,7 +69,7 @@ window.onload = function init()
 
     // vertex color
 
-    var colorBuffer = gl.createBuffer();
+    colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
@@ -71,7 +79,7 @@ window.onload = function init()
 
     // element index
 
-    var indexBuffer = gl.createBuffer();
+    indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(index), gl.STATIC_DRAW);
 
@@ -83,8 +91,24 @@ window.onload = function init()
 function render()
 {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    // gl.drawArrays(gl.POINTS, 0, vertices.length);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(index), gl.STATIC_DRAW);
+
     gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+    window.requestAnimFrame(render);
 }
 
 /**
@@ -92,20 +116,24 @@ function render()
  */
 // center is a vec3
 function genCirclePoints(center, r, az) {
-    var pv = [center];
+    var newCenter = center;
+    newCenter[2] = az;
+    var pv = [newCenter];
     var d = Math.PI * (360 / numPoints) / 180;
     for (var theta = 0; theta < 2 * Math.PI; theta += d) {
 	var x = center[0] + r * Math.cos(theta);
 	var y = center[1] + r * Math.sin(theta);
 	var z = az;
-	pv.push(vec3(x, y, z));
+	if (pv.length <= numPoints) {
+	    pv.push(vec3(x, y, z));
+	}
     }
     return pv;
 }
 
-function genCircleIndex() {
+function genCircleIndex(pv) {
     var iv = [];
-    for (var i = 1; i < numPoints; i++) {
+    for (var i = 1; i < pv.length - 1; i++) {
 	iv.push(0);
 	iv.push(i);
 	iv.push(i+1);
@@ -122,17 +150,19 @@ function Circle(center, radius, colorIndex, az) {
     this.y = center[1];
     this.r = radius;
     this.points = genCirclePoints(center, radius, az); // points on the peripheral
-    this.index = genCircleIndex();
+    this.index = genCircleIndex(this.points);
     this.color = new Array(this.points.length);
-    for (i = 0; i < this.points.length; i++) {
+    for (var i = 0; i < this.points.length; i++) {
 	this.color[i] = baseColors[colorIndex];
     }
 }
 
 function concatIndex(a, b) {
-    var d = a.length / 3 + 1;
-    for (var i = 0; i < b.length; i++) {
-	b[i] += d;
+    if (a.length > 1) {
+	var d = a[a.length - 2] + 1;
+	for (var i = 0; i < b.length; i++) {
+	    b[i] += d;
+	}
     }
     return Array.prototype.concat.apply(a, b);
 }
@@ -142,4 +172,16 @@ function addObject(obj) {
     vertices = vertices.concat(obj.points);
     index = concatIndex(index, obj.index);
     colors = colors.concat(obj.color);
+    // console.log(vertices);
+    // console.log(index);
+    // console.log(colors);
+}
+
+function genBacteria(disc) {
+    if (bactIndex < maxBacts) {
+	var i1 = bactIndex;
+	var b1 = new Circle(disc.points[bactIndex], 0.05, 8, -1);
+	addObject(b1);
+	bactIndex++;
+    }
 }
