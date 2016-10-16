4049a5b3f0d18635325ecf7c40f0e57c7bb9e549
Ryan Duan <duanpanda@gmail.com>
2016-10-15 23:10:20 -0400

    private JS buffer, global GL buffer, addGLObj() system

--- bugzapper.js
+++ bugzapper.js
@@ -2,9 +2,8 @@ var canvas;
 var gl;
 var program;
 
-var vertexBuffer = null;
-var colorBuffer = null;
-var indexBuffer = null;
+var vBuf = null;
+var cBuf = null;
 
 var baseColors = [
     vec3(0.0, 0.0, 0.0), // black
@@ -20,15 +19,12 @@ var baseColors = [
 
 var maxNumTriangles = 5000;
 var maxNumVertices = 3 * maxNumTriangles;
-var vertexBufferSize = Float64Array.BYTES_PER_ELEMENT * 2 * maxNumVertices;
+const BYTES_PER_VERTEX = Float32Array.BYTES_PER_ELEMENT * 2;
+const BYTES_PER_VERTEX_COLOR = Float32Array.BYTES_PER_ELEMENT * 3;
+var vertexBufferSize = BYTES_PER_VERTEX * maxNumVertices;
 var colorBufferSize = vertexBufferSize;
-var indexBufferSize = Uint16Array.BYTES_PER_ELEMENT * 3 * maxNumVertices;
-
-// The following resources are shared among the disk and all the bacterias.
-var thetaList = [];
-var vertices = [];
-var indices = [];
-var colors = [];
+// vertex index in GL vertex buffer, count and reference vertices
+var vIndex = 0;
 
 // attributes that configure the game and the game objects
 var rDisk = 0.7;
@@ -40,16 +36,10 @@ var diskColorIndex = 7;
 var gameTicks = 1;
 var maxNumBact = 10;
 var maxDt = 15;
-var interval = 10;
+var intervalId = 0;
 
 // game objects
-var diskObj = null;
-var bacteriaList = [];
-
-var diskIndice = [];
-var bacteriaIndice = [];
-
-var intervalId = 0;
+var objs = [];
 
 window.onload = function init()
 {
@@ -58,47 +48,32 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
-    //
-    //	Configure WebGL
-    //
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    //	Load shaders and initialize attribute buffers
     program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    // Allocate VBOs in GPU and set Attributes to point to VBOs
-
-    // vertex coordinates
-    vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexBufferSize, gl.STATIC_DRAW);
-
     var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    // vertex color
-    colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, colorBufferSize, gl.STATIC_DRAW);
-
     var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
-    // element indices
-    indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexBufferSize, gl.STATIC_DRAW);
+    vBuf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexBufferSize, gl.STATIC_DRAW);
 
-    //
-    //	Initialize our data for the disk and bacteria
-    //
+    cBuf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, colorBufferSize, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuf);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuf);
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+
     initObjData();
-
-    updateGLBuffers();
 
     canvas.addEventListener("mousedown", function(event) {
 	var x = event.clientX;
@@ -118,25 +93,37 @@ window.onload = function init()
 	}
     });
 
+    intervalId = window.setInterval(updateGame, 150);
+
     render();
 };
 
 function initObjData()
 {
-    thetaList = genGlobalThetaList();
-    vertices = genGlobalVertices(thetaList, rDisk, rCrustInner, rCrustOuter);
-    indices = genGlobalIndice();
-    colors = genGlobalColorBuffer();
-    clearGlobalColorBuffer();
-    colors = setObjColor(diskIndice, baseColors, diskColorIndex);
-    var sector = 360 / maxNumBact;
-    var theta = getRandomInt(0, 1 * sector);
-    console.log(theta);
-    var color = rd_rem(0, 5) + 1;
-    bacteriaList[0] = new Bacteria(theta, 1, color);
-    colors = setObjColor(bacteriaList[0].getIndice(),
-			 baseColors, bacteriaList[0].color);
-    intervalId = window.setInterval(updateGame, 150);
+    var disk = new Disk(0.0, 0.0, rDisk, vec3(0.7, 0.9, 0.3));
+    console.log(disk);
+    objs.push(disk);
+    for (var i = 0; i < maxNumBact; i++) {
+	var b = new Bacteria(0, 30, baseColors[8]);
+	objs.push(b);
+    }
+    vIndex = 0;
+    for (var j = 0; j < objs.length; j++) {
+	addGLObj(objs[j]);
+    }
+}
+
+function addGLObj(obj)
+{
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuf);
+    var v = flatten(obj.vertices);
+    gl.bufferSubData(gl.ARRAY_BUFFER, vIndex * BYTES_PER_VERTEX, v);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuf);
+    var c = flatten(obj.colors);
+    gl.bufferSubData(gl.ARRAY_BUFFER, vIndex * BYTES_PER_VERTEX_COLOR, c);
+
+    vIndex += obj.vertices.length;
 }
 
 function updateGame()
@@ -147,176 +134,17 @@ function updateGame()
 	gameTicks = 1;
 	return;
     }
-    clearGlobalColorBuffer();
-    colors = setObjColor(diskIndice, baseColors, diskColorIndex);
-    for (var i = 0; i < bacteriaList.length; i++) {
-	var olddt = bacteriaList[i].dt;
-	if (olddt < maxDt) {
-	    var newdt = olddt + 1;
-	    bacteriaList[i].update(bacteriaList[i].t, newdt);
-	}
-	colors = setObjColor(bacteriaList[i].getIndice(),
-			     baseColors, bacteriaList[i].color);
-    }
-    if (gameTicks % interval == 0) {
-	// add a new bacteria
-	var n = bacteriaList.length;
-	if (n < maxNumBact) {
-	    var sector = 360 / maxNumBact;
-	    var theta = getRandomInt(n * sector, (n+1) * sector);
-	    console.log(theta);
-	    var color = rd_rem(n, 5) + 1;
-	    bacteriaList[n] = new Bacteria(theta, 1, color);
-	    colors = setObjColor(bacteriaList[n].getIndice(),
-				 baseColors, bacteriaList[n].color);
-	}
-    }
-    updateGLBuffers();
-}
-
-function updateGLBuffers()
-{
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(colors));
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferSubData(gl.ELEMENT_ARRAY_BUFFER, 0, new Uint16Array(indices));
 }
 
 function render()
 {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+    var vIndex = 0;      // vertex index in vertex buffer and color buffer
+    for (var i = 0; i < objs.length; i++) {
+	gl.drawArrays(objs[i].drawMode, vIndex, objs[i].vertices.length);
+	vIndex += objs[i].vertices.length;
+    }
     window.requestAnimFrame(render);
-}
-
-/**
- * Generate a theta list that maps to all the vertices.
- * In Degrees.
- */
-function genGlobalThetaList()
-{
-    var t = [];
-    var d = 1;
-    for (var i = 0; i < 360; i = i + d) {
-	t.push(i);
-    }
-    return t;
-}
-
-/**
- * Generate all vertices that are used in this program.
- * Put the origin point to the end.
- * Return a vec2 list.
- */
-function genGlobalVertices(thetaList, r1, r2, r3)
-{
-    var t = [];
-    thetaList.forEach(function(theta, index, array) {
-	var ra = theta * Math.PI / 180; // degrees to radians
-	var p1x = r1 * Math.cos(ra);
-	var p1y = r1 * Math.sin(ra);
-	var p2x = r2 * Math.cos(ra);
-	var p2y = r2 * Math.sin(ra);
-	var p3x = r3 * Math.cos(ra);
-	var p3y = r3 * Math.sin(ra);
-	t.push(vec2(p1x, p1y));
-	t.push(vec2(p2x, p2y));
-	t.push(vec2(p3x, p3y));
-    });
-    t.push(vec2(0.0, 0.0));
-    return t;
-}
-
-function genGlobalIndice()
-{
-    diskIndice = genDiskTriangles(thetaList, vertices);
-    bacteriaIndice = genBacteriaTriangles(thetaList.concat([0]));
-    return indices = diskIndice.concat(bacteriaIndice);
-}
-
-function genGlobalColorBuffer()
-{
-    return colors = new Array(vertices.length);
-}
-
-function clearGlobalColorBuffer()
-{
-    for (var i = 0; i < vertices.length; i++) {
-	colors[i] = baseColors[0];
-    }
-}
-
-/**
- * Return an index list.  The index references the elements in the vertex list.
- */
-function genDiskTriangles(thetaList, vertices)
-{
-    var p = [];
-    var originIndex = vertices.length - 1;
-    for (var i = 0; i < thetaList.length - 1; i++) {
-	var j = 3 * i;
-	p.push(originIndex);
-	p.push(j);		// 3*i
-	p.push(j+3);		// 3*(i+1)
-    }
-    p.push(originIndex);
-    p.push(3 * i);
-    p.push(0);
-    return p;
-}
-
-/**
- * In the global thetaList, find the best matched one, and return its index.
- */
-function getMatchedThetaIndex(t)
-{
-    var index = thetaList.indexOf(t);
-    if (index == -1) {
-	// not found
-	// TODO
-    }
-    return index;
-}
-
-function getThetaIndexPair(t1, t2)
-{
-    var begin = getMatchedThetaIndex(t1);
-    var end = getMatchedThetaIndex(t2);
-    return [begin, end];
-}
-
-/**
- * Return an index list.  The index references the elements in vertex list.
- * ts is a theta index list that references the global theta list.
- */
-function genBacteriaTriangles(ts)
-{
-    var p = [];
-    for (var i = 0; i <= ts.length - 3; i += 2) {
-	var a = 3 * ts[i] + 1;
-	var b = 3 * ts[i] + 2;
-	var c = 3 * ts[i+1] + 1;
-	var d = 3 * ts[i+1] + 2;
-	var e = 3 * ts[i+2] + 1;
-	var f = 3 * ts[i+2] + 2;
-	p.push(a); p.push(d); p.push(b);
-	p.push(a); p.push(c); p.push(d);
-	p.push(c); p.push(f); p.push(d);
-	p.push(c); p.push(e); p.push(f);
-    }
-    return p;
-}
-
-function setObjColor(vertexIndice, baseColors, colorIndex)
-{
-    vertexIndice.forEach(function(item, i, array) {
-	colors[item] = baseColors[colorIndex];
-    });
-    return colors;
 }
 
 function assert(condition, message)
@@ -340,19 +168,46 @@ function getRandomInt(min, max)
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-/**
- * GameObj is a special pointer to the global indice[]
- * which contains a set of triples that consists triangles.
- * Each GameObj points to a subset of indice[], which means
- * that subset belongs to this GameObj.
- *
- * indexBegin is the index at the beginning of the subset. (included)
- * indexEnd is the index at the end of the subset. (excluded)
- */
-function GameObj(indexBegin, indexEnd)
+function GameObj()
 {
-    this.vIndexBegin = indexBegin;
-    this.vIndexEnd = indexEnd;
+    this.vertices = [];
+    this.colors = [];
+    this.color = vec3(0.0, 0.0, 0.0);
+    this.setColor = function(c) {
+	this.color = c;
+	if (this.colors.length != this.vertices.length) {
+	    this.colors = new Array(this.vertices.length);
+	}
+	for (var i = 0; i < this.colors.length; i++) {
+	    this.colors[i] = c;
+	}
+    };
+}
+
+function Disk(x, y, r, c)
+{
+    GameObj.call(this);
+    this.x = x;
+    this.y = y;
+    this.radius = r;
+    this.drawMode = gl.TRIANGLE_FAN;
+
+    this.genCirclePoints = function() {
+	if (this.vertices.length != 362) {
+	    this.vertices = new Array(362);
+	}
+	this.vertices[0] = vec2(x, y);
+	for (var i = 0; i < 360; i++) {
+	    var t = i / 180 * Math.PI;
+	    var nx = x + r * Math.cos(t);
+	    var ny = y + r * Math.sin(t);
+	    this.vertices[i+1] = vec2(nx, ny);
+	}
+	this.vertices[361] = this.vertices[1];
+    };
+
+    this.genCirclePoints();
+    this.setColor(c);
 }
 
 // pre: 0 <= t0 <= 359, 0 <= dt <= 359, integers
@@ -363,43 +218,48 @@ function Bacteria(t, dt, color)
     assert((typeof t === 'number') && Math.floor(t) === t, 'must: t is integer');
     assert((typeof dt === 'number') && Math.floor(dt) === dt, 'must: dt is integer');
 
-    GameObj.call(0, 0);
+    GameObj.call(this);
     this.t = t;
     this.dt = dt;
-    this.color = color;
+    this.drawMode = gl.TRIANGLE_STRIP;
 
-    this.update = function(t, dt) {
+    this.genPoints = function() {
+	if (this.vertices.length != 31 * 2) {
+	    this.vertices = new Array(31 * 2);
+	}
+	for (var t = -maxDt, i = 0; t <= maxDt; t++, i += 2) {
+	    var tr = t * Math.PI / 180; // degrees to radians
+	    var p1x = rCrustInner * Math.cos(tr);
+	    var p1y = rCrustInner * Math.sin(tr);
+	    var p2x = rCrustOuter * Math.cos(tr);
+	    var p2y = rCrustOuter * Math.sin(tr);
+	    this.vertices[i] = vec2(p1x, p1y);
+	    this.vertices[i+1] = vec2(p2x, p2y);
+	}
+    };
+
+    this.genPoints();
+
+    this.rotate = function(t) {
 	this.t = t;
-	this.dt = dt;
-	var rangePair = rd_pair_rem([t-dt, t+dt], 360);
-	this.thetaBegin = rangePair[0];
-	this.thetaEnd = rangePair[1]; // included
-
-	// indexPair.end is included
-	var indexPair = getThetaIndexPair(this.thetaBegin, this.thetaEnd);
-
-	// thetaList.length is the number of vertices on the disk circle
-	// which is the first part in the global indices[].
-	this.vIndexBegin = indexPair[0] * 2;
-
-	// vIndexEnd is excluded
-	this.vIndexEnd = indexPair[1] * 2;
     };
 
-    this.update(t, dt);
-
-    this.getIndice = function() {
-	var a = this.vIndexBegin;
-	var b = this.vIndexEnd;
-	var indexRanges = rd_gen_ranges([a, b], bacteriaIndice.length / 3);
-	var indiceList = [];
-	indexRanges.forEach(function(range, index, array) {
-	    var begin = (range[0] + thetaList.length) * 3;
-	    var end = (range[1] + thetaList.length) * 3;
-	    indiceList = indiceList.concat(indices.slice(begin, end));
-	});
-	return indiceList;
+    this.grow = function(dt) {
+	// grow this.dt to dt, and update visible buffer section
     };
+
+    this.setColor = function(c) {
+	this.color = c;
+	if (this.colors.length != this.vertices.length) {
+	    this.colors = new Array(this.vertices.length);
+	}
+	for (var i = 0; i < this.colors.length; i++) {
+	    this.colors[i] = c;
+	}
+    };
+
+    this.rotate(t, dt);
+    this.setColor(color);
 }
 
 /**
